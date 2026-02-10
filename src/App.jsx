0fa9792c978dc3520e825df3c4a99c3ef61c93ec
Vinícius Santos <3izuna@gmail.com>
2026-02-09 23:06:58 -0300

    I'll sleep, I can do this other time

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import SideBar from "./components/sidebar/SideBar"
 import '../src/styles/global.css'
 import './styles/main.css'
-import { BiEuro } from "react-icons/bi";
-import { TbCurrencyDollar } from "react-icons/tb";
-import { PiCurrencyJpyBold } from "react-icons/pi";
-import { IoMdArrowRoundForward } from "react-icons/io";
+import ConvertionCoins from "./components/convertionCoins/ConvertionCoins"
+
+
 
 
 function App() {
@@ -17,52 +16,22 @@ function App() {
       <SideBar />
 
       <main id="main-content">
-        <div className="data-card" id="convertion-coins">
-          <h2>Conversão</h2>
-
-          <div className="local-explication-money">
-            <div className="money-explication">
-               <div className="type-money">EUR Euro</div>
-               <div className="type-money">USD Dollar</div>
-               <div className="type-money">BRL Real</div>
-               <div className="type-money">JPY Iene</div>
-            </div>
-
-            <div className="arrow-explications">
-              <IoMdArrowRoundForward size={21} />
-              <IoMdArrowRoundForward size={21} />
-              <IoMdArrowRoundForward size={21}/>
-              <IoMdArrowRoundForward size={21}/>
-            </div>
-
-            <div className="symbols-money">
-              <BiEuro size={25} />
-              <TbCurrencyDollar size={25}/>
-              R$
-              <PiCurrencyJpyBold size={25}/>
-            </div>
-
-          </div>
-        </div>
-
-
-
-
-
+        <ConvertionCoins />
 
         <div className="data-card" id="weather">
-          <h2>Clima</h2>
+          <h2 className="cards-tittle">Clima</h2>
         </div>
 
         <div className="data-card" id="timer-country">
-          <h2>hora de cada pais atual</h2>
+          <h2 className="cards-tittle">hora de cada pais atual</h2>
         </div>
 
         <div className="data-card" id="history">
-          <h3>Historico</h3>
+          <h3 className="cards-tittle">Historico</h3>
         </div>
+
         <div className="data-card" id="img-slide">
-          <h2>Slide img Country</h2>
+  
         </div>
         
       </main>
---
+++ src/components/convertionCoins/ConvertionCoins.jsx
@@ -0,0 +1,50 @@
+import { IoMdArrowRoundForward } from "react-icons/io";
+import { BiEuro } from "react-icons/bi";
+import { TbCurrencyDollar } from "react-icons/tb";
+import { PiCurrencyJpyBold } from "react-icons/pi";
+import '../convertionCoins/convertion-coins.css'
+
+const ConvertionCoins = () => {
+
+    
+  return (
+    <>
+        <div className="data-card" id="convertion-coins">
+          <h2 className="cards-tittle">Conversão</h2>
+
+          <div className="local-explication-money">
+            <div className="money-explication">
+               <div className="type-money">EUR Euro</div>
+               <div className="type-money">USD Dollar</div>
+               <div className="type-money">BRL Real</div>
+               <div className="type-money">JPY Iene</div>
+            </div>
+
+            <div className="arrow-explications">
+              <IoMdArrowRoundForward size={21} />
+              <IoMdArrowRoundForward size={21} />
+              <IoMdArrowRoundForward size={21}/>
+              <IoMdArrowRoundForward size={21}/>
+            </div>
+
+            <div className="symbols-money">
+              <BiEuro size={25} />
+              <TbCurrencyDollar size={25}/>
+              R$
+              <PiCurrencyJpyBold size={25}/>
+            </div>
+
+          </div>
+
+
+          <div className="area-convertion">
+            
+          </div>
+        </div>
+
+   
+    </>
+  )
+}
+
+export default ConvertionCoins
