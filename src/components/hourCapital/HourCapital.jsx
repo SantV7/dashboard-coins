105a86332398dce47a553843af4d6f9f974cf531
Vinícius Santos <3izuna@gmail.com>
2026-02-11 13:20:41 -0300

    Set Hour

--- src/components/hourCapital/HourCapital.jsx
+++ src/components/hourCapital/HourCapital.jsx
@@ -3,10 +3,10 @@ import '../hourCapital/hour-capital.css'
 
 const HourCapital = () => {
 
-    const [hourBrasilia, setHourBrasilia] = useState(new Date().getHours())
-    const [hourTokyo, setTokyo] = useState(new Date().getHours())
-    const [hourWashington, setHourWashington] = useState(new Date().getHours())
-    const [hourBrussels, setHourBrussels] = useState(new Date().getHours())
+    const [hourBrasilia, setHourBrasilia] = useState((new Date().getUTCHours() - 3 + 24) % 24)
+    const [hourTokyo, setTokyo] = useState((new Date().getUTCHours() + 9 + 24) % 24)
+    const [hourWashington, setHourWashington] = useState((new Date().getUTCHours() - 5 + 24) % 24)
+    const [hourBrussels, setHourBrussels] = useState((new Date().getUTCHours() + 1 + 24) % 24)
 
     const [minutes, setMinutes] = useState(new Date().getMinutes())
     const [seconds, setSeconds] = useState(new Date().getSeconds())      
@@ -17,9 +17,9 @@ const HourCapital = () => {
             const nowGetTime = new Date()
 
             setHourBrasilia((nowGetTime.getUTCHours() - 3 + 24) % 24 )
-            setTokyo((nowGetTime.getUTCHours() + 9) % 24) 
-            setHourWashington((nowGetTime.getUTCHours() + 1) % 24)
-            setHourBrussels((nowGetTime.getUTCHours() + 1) % 24)
+            setTokyo((nowGetTime.getUTCHours() + 9 + 24) % 24) 
+            setHourWashington((nowGetTime.getUTCHours() - 5 + 24) % 24)
+            setHourBrussels((nowGetTime.getUTCHours() + 1 + 24) % 24)
 
             setMinutes(nowGetTime.getMinutes())
             setSeconds(nowGetTime.getSeconds())
@@ -35,42 +35,34 @@ const HourCapital = () => {
      <h3 className="cards-tittle">Hora da capital</h3>
     <div className='hour-capital-area'>
         <div id='capital-name'>
-            <div>Tokyo</div>
-            <div>Brasília</div>
-            <div>Washington</div>
-            <div>Brussels</div>
+            <div className='name'>Tokyo</div>
+            <div className='name'>Brasília</div>
+            <div className='name'>Washington</div>
+            <div className='name'>Brussels</div>
         </div>
 
         <div id='hour-capital'>
-            <div>
+            <div className='time-to-capital'>
                 {
-                 `${hourTokyo.toString().padStart('2', 0)}:
-                  ${minutes.toString().padStart('2', 0)}:
-                  ${seconds.toString().padStart('2', 0)}h
+                 `${hourTokyo.toString().padStart('2', 0)}:${minutes.toString().padStart('2', 0)}:${seconds.toString().padStart('2', 0)}h
                `}                
             </div>
 
-            <div>
+            <div className='time-to-capital'>
                 {
-                 `${hourBrasilia.toString().padStart('2', 0)}:
-                  ${minutes.toString().padStart('2', 0)}:
-                  ${seconds.toString().padStart('2', 0)}h
+                 `${hourBrasilia.toString().padStart('2', 0)}:${minutes.toString().padStart('2', 0)}:${seconds.toString().padStart('2', 0)}h
                `}
             </div>
 
-            <div>
+            <div className='time-to-capital'>
                 {
-                 `${hourWashington.toString().padStart('2', 0)}:
-                  ${minutes.toString().padStart('2', 0)}:
-                  ${seconds.toString().padStart('2', 0)}h
+                 `${hourWashington.toString().padStart('2', 0)}:${minutes.toString().padStart('2', 0)}:${seconds.toString().padStart('2', 0)}h
                `}               
             </div>
 
-            <div>
+            <div className='time-to-capital'>
                 {
-                 `${hourBrussels.toString().padStart('2', 0)}:
-                  ${minutes.toString().padStart('2', 0)}:
-                  ${seconds.toString().padStart('2', 0)}h
+                 `${hourBrussels.toString().padStart('2', 0)}:${minutes.toString().padStart('2', 0)}:${seconds.toString().padStart('2', 0)}h
                `}               
             </div>
         </div>
