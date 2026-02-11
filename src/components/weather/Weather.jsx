efbf3727b50269f3556c06f54fe793f9567040b5
Vinícius Santos <3izuna@gmail.com>
2026-02-11 14:39:16 -0300

    Creating Climate area

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,17 +1,24 @@
+import { useState } from 'react'
 import '../weather/weather.css'
+import { TiWeatherCloudy } from "react-icons/ti";
 
 const Weather = () => {
+
+  const [weatherTokyo, setWeatherTokyo] = useState(null)
+  const [weatherBrasilia, setWeatherBrasilia] = useState(null)
+  const [weatherWashington, setWeatherWashington] = useState(null)
+  const [weatherBrussels, setWeatherBrussels] = useState(null)
 
 
   return (
     <>
-    <h2 className="cards-tittle">Clima</h2>
+    <h2 id='weather-h2' className="cards-tittle">Clima <TiWeatherCloudy size={54}/></h2>
 
     <div className="weather-location">
-        <div className="jp-capital">Tokyo - JP - icon do clima atual - 2°C</div>
-        <div className="brasil-capital">Brasília - BR - icon do clima atual - 25°C</div>
-        <div className="washington-capital">Washington - US - icon do clima atual - 29°C</div>
-        <div className="brussels-capital">Brussels - BE - icon do clima atual - 17°C</div>
+        <div className='climate-capital-name' id="jp-capital">Tokyo - JP  <div className="climate-temperature">2°C</div></div>
+        <div className='climate-capital-name' id="brasil-capital">Brasília - BR  <div className="climate-temperature">25°C</div></div>
+        <div className='climate-capital-name' id="washington-capital">Washington - US  <div className="climate-temperature">29°C</div></div>
+        <div className='climate-capital-name' id="brussels-capital">Brussels - BE  <div className="climate-temperature">17°C</div></div>
     </div>
     </>
   )
