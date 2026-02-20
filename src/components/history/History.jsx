854e55495eb1818ddff126803d42179950fb9718
Vinícius Santos <3izuna@gmail.com>
2026-02-20 15:26:18 -0300

    I'll try do this to later

--- src/components/history/History.jsx
+++ src/components/history/History.jsx
@@ -1,33 +1,10 @@
 import '../history/history.css'
 
-const History = ({ history }) => {
-  if (!history || history.length === 0) {
-    return (
-      <div className="history-card">
-        <h3 className="cards-tittle">Histórico</h3>
-        <p>Nenhum dado registrado ainda.</p>
-      </div>
-    )
-  }
-
+const History = () => {
   return (
-    <div className="history-card">
-      <h3 className="cards-tittle">Histórico</h3>
-
-      <div className="history-list">
-        {history.map((item, index) => (
-          <div key={index} className="history-item">
-            <span className="history-time">{item.time}</span>
-
-            <div className="history-values">
-              <span>EUR: {item.rates.EUR.toFixed(4)}</span>
-              <span>BRL: {item.rates.BRL.toFixed(4)}</span>
-              <span>JPY: {item.rates.JPY.toFixed(4)}</span>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+      <>
+        <h3 className="cards-tittle">Histórico</h3>
+      </>
   )
 }
 
